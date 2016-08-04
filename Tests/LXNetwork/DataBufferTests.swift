c6e73d0ca9fd91a77d5e12ad1ae0f83c6ade3450
Stan Chang Khin Boon <khinboon@gmail.com>
2016-08-04 22:57:48 +0800

    Adds a DataPipe unit test.

--- Tests/LXNetwork/DataBufferTests.swift
+++ Tests/LXNetwork/DataBufferTests.swift
@@ -3,7 +3,19 @@ import XCTest
 
 class DataBufferTests: XCTestCase {
     func testExample() {
-        XCTFail("Not Implemented…")
+        let dataBuffer = DataBuffer()
+        dataBuffer.asyncIn(data: "Hello, ".data(using: .utf8)!)
+        dataBuffer.asyncIn(data: "World!".data(using: .utf8)!)
+        
+        let expectation = self.expectation(description: "Async IO operations is deterministic…")
+        dataBuffer.asyncOut(handler: { (data) -> DataBuffer.OutResult in
+            defer {
+                expectation.fulfill()
+            }
+            XCTAssertEqual(String(data: data, encoding: .utf8)!, "Hello, World!")
+            return .Consume(bytes: data.count)
+        })
+        self.waitForExpectations(timeout: 5.0)
     }
 
     static var allTests : [(String, (DataBufferTests) -> () throws -> Void)] {
